eeac346179ddd84e85616bc310fb6176a8c1a8d8
Nikita Kasatkin <nikita.kasatkin@exactprosystems.com>
2017-06-12 20:38:56 +0300

    first commit

---
+++ src/main/java/com/project/Application.java
@@ -0,0 +1,30 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * NG.Kasatkin 11.06.2017
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
